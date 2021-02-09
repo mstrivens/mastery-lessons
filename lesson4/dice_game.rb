5621d8e508f0d219cd5650a703580e0f7576f79a
Max <max.strivens@live.co.uk>
2021-02-09 21:01:27 +0000

    Adding lesson 4 pratices

---
+++ lesson4/dice_game.rb
@@ -0,0 +1,26 @@
+def dice
+  rand(6)
+end
+
+player_1 = 0
+player_2 = 0
+
+
+  while (player_1 < 2 && player_2 < 2)
+    player_1_total = dice + dice
+    player_2_total = dice + dice
+
+
+    if player_1_total > player_2_total
+      player_1 += 1
+    else player_2_total > player_1_total
+      player_2 += 1
+    end
+  end
+
+
+  if player_1 == 2
+    puts "Player 1 is victorious!"
+  else
+    puts "Player 2 is the winner!"
+  end
---
+++ lesson4/while-loop.rb
@@ -0,0 +1,11 @@
+
+my_number = 1
+
+  while my_number < 100
+
+      if my_number.odd?
+        puts my_number
+      end
+
+      my_number += 1
+end
